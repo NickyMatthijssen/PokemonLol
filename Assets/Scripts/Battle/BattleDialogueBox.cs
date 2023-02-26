41db54015475a3a516f404b07c03a6069b19ced3
NickyMatthijssen <36468434+NickyMatthijssen@users.noreply.github.com>
2023-02-26 03:42:01 +0100

    Removed old base files.

--- Assets/Scripts/Battle/BattleDialogueBox.cs
+++
@@ -1,9 +0,0 @@
-using TMPro;
-using UnityEngine;
-
-public class BattleDialogueBox : MonoBehaviour
-{
-    [SerializeField] private TMP_Text text;
-
-    public void SetText(string value) => text.text = value;
-}
--- Assets/Scripts/Battle/IBattleAction.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public interface IBattleAction
-{
-    public int priority { get; }
-    
-    public void Execute();
-}
